07047ae7a7ab159d845e0411c672cd304f9fba71
ngchongpeng <ngchongpeng@hotmail.com>
2021-06-13 19:48:57 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HistoryComponent } from './history/history.component';
+import { HistoryDetailsComponent } from './history-details/history-details.component';
+import { MapComponent } from './map/map.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'history-details', component: HistoryDetailsComponent },
+  { path: 'map', component: MapComponent },
+  { path: '', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SliderComponent } from './slider/slider.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HistoryComponent } from './history/history.component';
+import { HistoryDetailsComponent } from './history-details/history-details.component';
+import { MapComponent } from './map/map.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    AboutComponent,
+    ContactComponent,
+    HistoryComponent,
+    HistoryDetailsComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
